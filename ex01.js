a1dd5f534b4ac0fc5928c33a8e7a52303bb82125
ninoonworarat <d51765@ccollege.ac.th>
2023-07-11 12:16:08 +0700

    u

--- ex01.js
+++ ex01.js
@@ -2,3 +2,16 @@ var t = setInterval(function() {
   console.log('test2')
 }, 1000)
 console.log('test1')
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! WORARAT')
+})
+
+app.listen(port, () => {
+  console.log(` on port ${port}`)
+  console.log(`IT CHAIYAPUM`)
+})
